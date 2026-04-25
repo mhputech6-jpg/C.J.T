d9e4bd50ddd09791291e58452d9e021723ff6b4a
mhputech6-jpg <mhputech6@gmail.com>
2026-04-26 01:19:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,22 @@
-const CACHE = "cjt-v1";
+const CACHE = "cjt-v2";
+
+const URLS = [
+  "/C.J.T/",
+  "/C.J.T/index.html",
+  "/C.J.T/manifest.json",
+  "/C.J.T/icon-192.png",
+  "/C.J.T/icon-512.png"
+];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(cache => {
-      return cache.addAll([
-        "/C.J.T/",
-        "/C.J.T/index.html"
-      ]);
-    })
+    caches.open(CACHE).then(cache => cache.addAll(URLS))
   );
+});
+
+self.addEventListener("activate", e => {
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", e => {
