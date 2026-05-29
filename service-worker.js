8baefa39e20102a5b8ac4849c44235744ae6d7d1
mhputech6-jpg <mhputech6@gmail.com>
2026-05-30 00:03:16 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,48 +1,27 @@
-const CACHE = "cjt-v3";
+const CACHE = "cjt-v2";
 
 const URLS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+"/C.J.T/",
+"/C.J.T/index.html",
+"/C.J.T/manifest.json",
+"/C.J.T/icon-192.png",
+"/C.J.T/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => {
-      return cache.addAll(URLS);
-    })
-  );
+self.addEventListener("install", e => {
+self.skipWaiting();
+e.waitUntil(
+caches.open(CACHE).then(cache => cache.addAll(URLS))
+);
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys
-          .filter((key) => key !== CACHE)
-          .map((key) => caches.delete(key))
-      )
-    )
-  );
-
-  self.clients.claim();
+self.addEventListener("activate", e => {
+self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cached) => {
-      return (
-        cached ||
-        fetch(event.request).then((response) => {
-          return response;
-        })
-      );
-    })
-  );
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(res => res || fetch(e.request))
+);
 });
+
